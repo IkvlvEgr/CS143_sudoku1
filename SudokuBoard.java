9d678d98f9dce5c61f20b7ac23e333454f833a1f
IkvlvEgr <egor.iakovlev@go.shoreline.edu>
2025-04-20 15:04:40 -0700

    Part 2 - reading initial file

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,8 +1,28 @@
+import java.io.File;
+import java.util.*;
+import java.io.FileNotFoundException;
+
 public class SudokuBoard {
    private char[][] board;
    
    public SudokuBoard(String fileName) {
-   
+      board = new char[9][9];
+      
+      try {
+         Scanner scanner = new Scanner(new File(fileName));
+         int row = 0;
+         
+         while(scanner.hasNextLine() && row < 9) {
+            String line = scanner.nextLine();
+            
+            for(int col = 0; col < 9; col++) { 
+               board[row][col] = line.charAt(col);
+            }
+            row++;
+         }
+      } catch(FileNotFoundException e) {
+         System.out.println("File not found: " + fileName);
+      }
    }
    
    public String toString() {
