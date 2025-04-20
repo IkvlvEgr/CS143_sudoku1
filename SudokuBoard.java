1d575209c74dae9c347a76be6ebb4ec4d025c217
IkvlvEgr <egor.iakovlev@go.shoreline.edu>
2025-04-20 15:35:13 -0700

    Part 3 - toString method

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -26,6 +26,15 @@ public class SudokuBoard {
    }
    
    public String toString() {
-      return "";
+      String result = "";
+      
+      for(int row = 0; row < board.length; row++) {
+         for(int col = 0; col < board[row].length; col++) {
+            result += board[row][col];
+         }
+         result += "\n";
+      }
+      
+      return result;
    }
 }
